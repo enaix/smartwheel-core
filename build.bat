56710b169a926e7b3c6dccf2c02d590770733e8b
enaix <enaix@protonmail.com>
2023-03-17 11:48:23 +0300

    Fix windows build workflow

--- build.bat
+++ build.bat
@@ -1,8 +1,7 @@
-pip install nuitka pycoreutils
+pip install nuitka
 
 copy entrypoint.py src\main.py
 
 cd src
 
-# Idk how to install coreutils on gh actions...
-python -m pycoreutils yes | python -m nuitka --standalone --follow-imports --include-module=smartwheel.actions --include-module=smartwheel.backgrounds --include-module=smartwheel.serialpipe --include-module=smartwheel.settings_handlers --include-module=smartwheel.ui --include-module=smartwheel.ui.internal --enable-plugin=pyqt6 --include-data-dir=.\smartwheel=smartwheel --disable-console .\main.py
+python -c "while True: print('Yes')" | python -m nuitka --standalone --follow-imports --include-module=smartwheel.actions --include-module=smartwheel.backgrounds --include-module=smartwheel.serialpipe --include-module=smartwheel.settings_handlers --include-module=smartwheel.ui --include-module=smartwheel.ui.internal --enable-plugin=pyqt6 --include-data-dir=.\smartwheel=smartwheel --disable-console .\main.py
