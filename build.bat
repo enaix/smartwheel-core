9c63fb2f96ff68a1c0f0a4d25b3cc3eda5fd45a2
enaix <64410733+enaix@users.noreply.github.com>
2023-03-17 08:02:49 +0000

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,7 +1,8 @@
-pip install nuitka
+pip install nuitka pycoreutils
 
 copy entrypoint.py src\main.py
 
 cd src
 
-echo Yes|python -m nuitka --standalone --follow-imports --include-module=smartwheel.actions --include-module=smartwheel.backgrounds --include-module=smartwheel.serialpipe --include-module=smartwheel.settings_handlers --include-module=smartwheel.ui --include-module=smartwheel.ui.internal --enable-plugin=pyqt6 --include-data-dir=.\smartwheel=smartwheel --disable-console .\main.py
+# Idk how to install coreutils on gh actions...
+python -m pycoreutils yes | python -m nuitka --standalone --follow-imports --include-module=smartwheel.actions --include-module=smartwheel.backgrounds --include-module=smartwheel.serialpipe --include-module=smartwheel.settings_handlers --include-module=smartwheel.ui --include-module=smartwheel.ui.internal --enable-plugin=pyqt6 --include-data-dir=.\smartwheel=smartwheel --disable-console .\main.py
